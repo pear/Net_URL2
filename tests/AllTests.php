d3594cc452180c8597022ee7521faff3ca53746f
David Coallier <davidc@php.net>
2007-07-04 14:36:08 +0000

    - Unit tests by Brett Bieber
    git-svn-id: https://svn.php.net/repository/pear/packages/Net_URL2/trunk@239160 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Net_URL2_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+
+require_once 'Net/URL2Test.php';
+
+
+class Net_URL2_AllTests
+{
+    public static function main()
+    {
+
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Net_URL2Test');
+        /** Add testsuites, if there is. */
+        $suite->addTestSuite('Net_URL2Test');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Net_URL2_AllTests::main') {
+    Net_URL2_AllTests::main();
+}
+?>
