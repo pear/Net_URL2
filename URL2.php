15a9c171d84848b86f5b87a653da67ee527e7432
David Coallier <davidc@php.net>
2008-03-22 20:36:04 +0000

    - Copyrighted some years that are more plausible :)
    git-svn-id: https://svn.php.net/repository/pear/packages/Net_URL2/trunk@255820 c90b9560-bf6c-de11-be94-00142212c4b1

--- URL2.php
+++ URL2.php
@@ -1,6 +1,6 @@
 <?php
 // +-----------------------------------------------------------------------+
-// | Copyright (c) 2002-2004, Christian Schmidt, Peytz & Co. ApS            |
+// | Copyright (c) 2007-2008, Christian Schmidt, Peytz & Co. ApS            |
 // | All rights reserved.                                                  |
 // |                                                                       |
 // | Redistribution and use in source and binary forms, with or without    |
