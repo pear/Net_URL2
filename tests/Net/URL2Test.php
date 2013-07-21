pear/Net_URL2

commit 13e769b8d234549a80aa92ad5e39a6c77fe53103

Fix: URL normalization not as in RFC 3986
Net_URL2 did not properly address URL normilization as
outlined in RFC 3986 Section 6.

The normalize() method had two missing rungs in the
comparison ladder:

Percent-Encoding Normalization (6.2.2.2.) was not
addressed with the result that percent-encoded octets
that do not require percent-encoding were not normalized
to their unreserved character.

Scheme-Based Normalization (6.2.3.) was broken for the
default port in form of an empty string. This flaw was
the result of a missing string check.

These two issues are adressed with these fixes.

Steps done:

- Test extended with examples from RFC 3986 Section 6
- Fixed percent encoding in class Net_Url2 _normalizeCallback() method
- Fixed missing type-check in class Net_Url2 normalize() method
- Improved by using an array instead of foreach'ing over it
- Typo fixed; RFC number is 3986 (previously wrongly 3886)
- WS Fixed

Net/URL2.php
@@ -626,7 +626,7 @@ class Net_URL2
      */
     public function normalize()
     {
-        // See RFC 3886, section 6
+        // See RFC 3986, section 6
 
         // Scheme is case-insensitive
         if ($this->_scheme) {
@@ -639,23 +639,21 @@ class Net_URL2
         }
 
         // Remove default port number for known schemes (RFC 3986, section 6.2.3)
-        if ($this->_port
+        if ('' === $this->_port
+            || $this->_port
             && $this->_scheme
             && $this->_port == getservbyname($this->_scheme, 'tcp')
         ) {
-
             $this->_port = false;
         }
 
         // Normalize case of %XX percentage-encodings (RFC 3986, section 6.2.2.1)
-        foreach (array('_userinfo', '_host', '_path') as $part) {
-            if ($this->$part) {
-                $this->$part = preg_replace_callback(
-                    '/%[0-9a-f]{2}/i', array('self', '_normalizeCallback'),
-                    $this->$part
-                );
-            }
-        }
+        // Normalize percentage-encoded unreserved character (RFC 3986, section 6.2.2.2)
+        list($this->_userinfo, $this->_host, $this->_path)
+            = preg_replace_callback(
+                '/%[0-9a-f]{2}/i', array('self', '_normalizeCallback'),
+                array($this->_userinfo, $this->_host, $this->_path)
+            );
 
         // Path segment normalization (RFC 3986, section 6.2.2.3)
         $this->_path = self::removeDotSegments($this->_path);
@@ -675,7 +673,7 @@ class Net_URL2
      */
     private static function _normalizeCallback($matches)
     {
-        return strtoupper($matches[0]);
+        return self::urlencode(urldecode($matches[0]));
     }
 
     /**

tests/Net/URL2Test.php
@@ -346,20 +346,55 @@ class Net_URL2Test extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * data provider of equivalent URL pairs.
+     *
+     * @return array
+     */
+    public function provideEquivalentUrlLists()
+    {
+        return array(
+            // String equivalence:
+            array('http://example.com/', 'http://example.com/'),
+
+            // Originally first dataset:
+            array('http://www.example.com/%9a', 'http://www.example.com/%9A'),
+
+            // Example from RFC 3986 6.2.2.:
+            array('example://a/b/c/%7Bfoo%7D', 'eXAMPLE://a/./b/../b/%63/%7bfoo%7d'),
+
+            // Example from RFC 3986 6.2.2.1.:
+            array('HTTP://www.EXAMPLE.com/', 'http://www.example.com/'),
+
+            // Example from RFC 3986 6.2.3.:
+            array('http://example.com',   'http://example.com/',
+                  'http://example.com:/', 'http://example.com:80/'),
+        );
+    }
+
+    /**
      * This is a coverage test to invoke the normalize()
      * method.
      *
      * @return void
+     *
+     * @dataProvider provideEquivalentUrlLists
      */
     public function testNormalize()
     {
-        $abnormal = 'http://www.example.com/%9a';
-        $normal   = 'http://www.example.com/%9A';
+        $urls = func_get_args();
 
-        $url = new Net_Url2($abnormal);
-        $url->normalize();
+        $this->assertGreaterThanOrEqual(2, count($urls));
 
-        $this->assertSame($normal, (string) $url);
+        $last = null;
+
+        foreach ($urls as $index => $url) {
+            $url = new Net_Url2($url);
+            $url->normalize();
+            if ($index) {
+                $this->assertEquals((string) $last, (string) $url);
+            }
+            $last = $url;
+        }
     }
 }
 

commit 41fdb0f0eca988ab23ede78d317ccbd6a769fac1

Fix: Setting an empty userinfo results in commercial at-sign ("@")
Net_URL2 did not properly address handling a zero-
length string for the user-information (userinfo in
short).

According to RFC 3986 the commercial at-sign ("@")
should only be used if the user information is
present.

An empty userinfo (zero-length-string) is not re-
presenting such a userinfo. Therefore the user-
info is not present.

The original report by Ross Perkins was pointing
to a regression introduced in 13e769b stringy-
fying a bool.

Steps done:

- Test extended setting userinfo to zero-length
  string by adding testSetEmptyUserinfo() method
- Fixed ::$_userinfo string handling in Net_URL2
  getAuthority() method

References:

- https://pear.php.net/bugs/bug.php?id=20013 (#20013)
- https://github.com/pear/Net_URL2/pull/3 (#3)

Net/URL2.php
@@ -340,7 +340,7 @@ class Net_URL2
 
         $authority = '';
 
-        if ($this->_userinfo !== false) {
+        if (strlen($this->_userinfo)) {
             $authority .= $this->_userinfo . '@';
         }
 

tests/Net/URL2Test.php
@@ -53,6 +53,21 @@ class Net_URL2Test extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Tests setting an empty userinfo part
+     * Also: Regression test for Bug #20013
+     * @link https://pear.php.net/bugs/bug.php?id=20013
+     */
+    public function testSetEmptyUserinfo() {
+        $url = new Net_URL2('http://@www.example.com/');
+        $this->assertSame('http://www.example.com/', $url->getURL());
+
+        $url = new Net_URL2('http://www.example.com/');
+        $this->assertSame('http://www.example.com/', $url->getURL());
+        $url->setUserinfo('');
+        $this->assertSame('http://www.example.com/', $url->getURL());
+    }
+
+    /**
      * Tests setQueryVariable().
      */
     public function testSetQueryVariable() {