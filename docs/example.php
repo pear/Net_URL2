691be8d2bcaa90127878a54321e0411c405a1267
Tom Klingenberg <tklingenberg@lastflood.net>
2013-12-28 01:35:51 +0100

    Reviewed: Fix CS, license text quirks in docs
    The docs folder did not comply with PEAR Coding Standards.
    
    The code has been fixed. The invalid header of the file has been
    condensed.
    
    Licensing terms have been preserved as a single file.
    
    The text had little difference with the online reference w/o
    semantical differences so that it has been streamlined to it.
    
    The following changes were done:
    
    1.) The names of the authors may not be used to endorse -> Neither the
    name of the Richard Heyes nor the names of his contributors may be used to endorse
    2.) COPYRIGHT OWNER -> COPYRIGHT HOLDER

--- docs/example.php
+++ docs/example.php
@@ -1,46 +1,35 @@
 <?php
-// +-----------------------------------------------------------------------+
-// | Copyright (c) 2002-2003, Richard Heyes                                     |
-// | All rights reserved.                                                  |
-// |                                                                       |
-// | Redistribution and use in source and binary forms, with or without    |
-// | modification, are permitted provided that the following conditions    |
-// | are met:                                                              |
-// |                                                                       |
-// | o Redistributions of source code must retain the above copyright      |
-// |   notice, this list of conditions and the following disclaimer.       |
-// | o Redistributions in binary form must reproduce the above copyright   |
-// |   notice, this list of conditions and the following disclaimer in the |
-// |   documentation and/or other materials provided with the distribution.|
-// | o The names of the authors may not be used to endorse or promote      |
-// |   products derived from this software without specific prior written  |
-// |   permission.                                                         |
-// |                                                                       |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
-// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
-// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
-// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
-// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
-// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
-// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
-// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
-// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
-// |                                                                       |
-// +-----------------------------------------------------------------------+
-// | Author: Richard Heyes <richard at php net>                            |
-// +-----------------------------------------------------------------------+
-// $Id$
+/**
+ * Net_URL2, a class representing a URL as per RFC 3986.
+ *
+ * PHP version 5
+ *
+ * This file contains code Copyright (c) 2002-2003, Richard Heyes,
+ * See BSD-3-CLAUSE-Heyes,
+ * Author: Richard Heyes <richard at php net>
+ *
+ * @category Networking
+ * @package  Net_URL2
+ * @author   Some Pear Developers <pear@php.net>
+ * @license  https://spdx.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @link     https://tools.ietf.org/html/rfc3986
+ */
+
 /**
 * This example will decode the url given and display its
 * constituent parts.
 */
-    error_reporting(E_ALL | E_STRICT);
 
-    include('Net/URL2.php');
+error_reporting(E_ALL | E_STRICT);
 
-    $url = new Net_URL2('https://example.com/pls/portal30/PORTAL30.wwpob_page.changetabs?p_back_url=http%3A%2F%2Fexample.com%2Fservlet%2Fpage%3F_pageid%3D360%2C366%2C368%2C382%26_dad%3Dportal30%26_schema%3DPORTAL30&foo=bar');
+require 'Net/URL2.php';
+
+$url = new Net_URL2(
+    'https://example.com/pls/portal30/PORTAL30.wwpob_page.changetabs?'
+    .'p_back_url=http%3A%2F%2Fexample.com%2Fservlet%2Fpage%3F_pageid%3D360'
+    .'%2C366%2C368%2C382%26_dad%3Dportal30%26_schema%3DPORTAL30&foo=bar'
+);
+
 ?>
 <html>
 <body>
@@ -65,7 +54,8 @@ Anchor.....: <?php echo $url->anchor;?>
 Full URL...: <?php echo $url->getUrl(); ?>
 
 
-Resolve path (.././/foo/bar/joe/./././../jabba): <b><?php echo $url->resolve('.././/foo/bar/joe/./././../jabba'); ?></b>
+Resolve path (.././/foo/bar/joe/./././../jabba): <b><?php
+        echo $url->resolve('.././/foo/bar/joe/./././../jabba'); ?></b>
 </pre>
 
 </body>
