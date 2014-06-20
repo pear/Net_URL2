pear/Net_URL2

commit 7e286742a07a8221ed483a8dce1fec67087c1686

Fixed #20159: Authority not terminated by slash
When building the URL, the authority was not terminated by a slash
when the path was not starting with a slash. This has been fixed.

Steps done:

 - Add Test 20159
 - Fixed Pear Bug #20159: Authority not terminated by slash by adding
   private function _buildAuthorityAndPath() that properly puts authority
   and path together.

Net/URL2.php
@@ -577,11 +577,7 @@ class Net_URL2
             $url .= $this->_scheme . ':';
         }
 
-        $authority = $this->getAuthority();
-        if ($authority !== false) {
-            $url .= '//' . $authority;
-        }
-        $url .= $this->_path;
+        $url .= $this->_buildAuthorityAndPath($this->getAuthority(), $this->_path);
 
         if ($this->_query !== false) {
             $url .= '?' . $this->_query;
@@ -592,6 +588,26 @@ class Net_URL2
         }
 
         return $url;
+    }
+
+    /**
+     * Put authority and path together, wrapping authority
+     * into proper separators/terminators.
+     *
+     * @param string|bool $authority authority
+     * @param string      $path      path
+     *
+     * @return string
+     */
+    private function _buildAuthorityAndPath($authority, $path)
+    {
+        if ($authority === false) {
+            return $path;
+        }
+
+        $terminator = ($path !== '' && $path[0] !== '/') ? '/' : '';
+
+        return '//' . $authority . $terminator . $path;
     }
 
     /**

commit 1a30faf4607f76bd1706af910a89a50230b81d2c

Fixed #20304: file:// URI gets crippled
Net_URL2 internally does not differ between an unset and an empty
authority.

This is an issue for URIs that have an implicit federated non-empty
default authority by their scheme as this is the case for "file" URI
scheme.

To get Net_URL2 still working while not changing all inner guts, a small
fix has been made to Net_URL2::getURL handling the special case for
file:// URI authorities.

This issue has been introduced in 7e28674:

    Fixed #20159: Authority not terminated by slash

ca. 7 months ago.

Net/URL2.php
@@ -687,7 +687,12 @@ class Net_URL2
             $url .= $this->_scheme . ':';
         }
 
-        $url .= $this->_buildAuthorityAndPath($this->getAuthority(), $this->_path);
+        $authority = $this->getAuthority();
+        if ($authority === false && strtolower($this->_scheme) === 'file') {
+            $authority = '';
+        }
+
+        $url .= $this->_buildAuthorityAndPath($authority, $this->_path);
 
         if ($this->_query !== false) {
             $url .= '?' . $this->_query;
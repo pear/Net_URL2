1a30faf4607f76bd1706af910a89a50230b81d2c
Tom Klingenberg <tklingenberg@lastflood.net>
2014-06-10 01:27:45 +0200

    Fixed #20304: file:// URI gets crippled
    Net_URL2 internally does not differ between an unset and an empty
    authority.
    
    This is an issue for URIs that have an implicit federated non-empty
    default authority by their scheme as this is the case for "file" URI
    scheme.
    
    To get Net_URL2 still working while not changing all inner guts, a small
    fix has been made to Net_URL2::getURL handling the special case for
    file:// URI authorities.
    
    This issue has been introduced in 7e28674:
    
        Fixed #20159: Authority not terminated by slash
    
    ca. 7 months ago.

--- Net/URL2.php
+++ Net/URL2.php
@@ -687,7 +687,12 @@ class Net_URL2
             $url .= $this->_scheme . ':';
         }
 
-        $url .= $this->_buildAuthorityAndPath($this->getAuthority(), $this->_path);
+        $authority = $this->getAuthority();
+        if ($authority === false && strtolower($this->_scheme) === 'file') {
+            $authority = '';
+        }
+
+        $url .= $this->_buildAuthorityAndPath($authority, $this->_path);
 
         if ($this->_query !== false) {
             $url .= '?' . $this->_query;
